4ad4da73268aab4e0fe550a487516db67953ac3d
ipastorl <alum.ipastorl.2021@iesalixar.org>
2022-02-28 01:37:46 +0100

    EJERCICIOS EN CASA

---
+++ Entregable3_3/src/Ejercicio3/MAIN.java
@@ -0,0 +1,10 @@
+package Ejercicio3;
+
+public class MAIN {
+    public static void main(String[] args) {
+        Empleado emp = new Empleado();
+        System.out.println(emp.calculaSalarioBruto(1,1500.10f,1   ));
+        System.out.println(emp.calculaSalarioNeto(-1500));
+
+    }
+}
